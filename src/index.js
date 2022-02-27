29030e8c3fea0fb44f8ce78b516c60b4551c6ea0
AlexeiKozovski <akoz413@mail.ru>
2022-02-27 21:54:31 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let reverseNumber = '';
+    let str = Math.abs(n).toString();
+    for (let i = str.length-1; i >= 0; i--){
+        reverseNumber = reverseNumber + str[i]
+    }
+    return reverseNumber
 }
